0131e449bd522eb0062c958c8f64df6eef737012
Michael Manhire <michaelmanhire@gmail.com>
2020-11-23 22:28:49 -0800

    Add whitelist to PurgeCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    purge: {
+        content: [
+            './vendor/laravel/jetstream/**/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/views/**/*.blade.php',
+        ],
+
+        options: {
+            whitelist: [
+                'bg-green-100',
+                'text-green-800',
+                'bg-blue-100',
+                'text-blue-800',
+                'bg-yellow-100',
+                'text-yellow-800',
+                'bg-teal-100',
+                'text-teal-800',
+                'bg-red-100',
+                'text-red-800',
+                'bg-pink-100',
+                'text-pink-800',
+                'bg-orange-100',
+                'text-orange-800',
+                'bg-purple-100',
+                'text-purple-800',
+            ],
+        },
+    },
 
     theme: {
         extend: {
