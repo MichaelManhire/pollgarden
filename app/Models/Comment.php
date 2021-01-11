4af3633d29b37789f54b302c5b181ebff8e517f9
Michael Manhire <michaelmanhire@gmail.com>
2021-01-10 19:02:29 -0800

    Make soft deletes cascade down through all models
    - Fix a bug where votes of deleted polls broke a user's profile.

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Comment extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes, CascadeSoftDeletes;
+
+    protected $cascadeDeletes = ['likes', 'replies'];
 
     /**
      * The attributes that aren't mass assignable.
--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Poll extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    use SoftDeletes, CascadeSoftDeletes;
+
+    protected $cascadeDeletes = ['comments', 'votes'];
 
     /**
      * Get the route key for the model.
